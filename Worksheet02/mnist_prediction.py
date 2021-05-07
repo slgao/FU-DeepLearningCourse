5cc1c5ea33208d718fa536030fef503a071840b2
sgao0001 <shulin.gao@eurailscout.com>
2021-05-07 23:12:58 +0200

    delete debug import

--- Worksheet02/mnist_prediction.py
+++ Worksheet02/mnist_prediction.py
@@ -20,7 +20,6 @@ import torch.optim as optim
 from torch.utils.data import Subset
 from sklearn.model_selection import train_test_split
 from torch.utils.data import TensorDataset, DataLoader
-import ipdb
 
 
 class Net(nn.Module):
