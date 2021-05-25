24b14820df7ffca82a9e03a3786631d7945b8061
sgao0001 <shulin.gao@eurailscout.com>
2021-05-25 17:40:05 +0200

    update code

--- Worksheet04/CIFAR_prediction.py
+++ Worksheet04/CIFAR_prediction.py
@@ -24,7 +24,16 @@ def plot_sample(X, y, index):
     plt.show()
 
 
-def save_images(X):
+def load_data():
+    with np.load('prediction-challenge-02-data.npz') as fh:
+        x_train = fh['x_train']
+        y_train = fh['y_train']
+        x_test = fh['x_test']
+    return x_train, y_train, x_test
+
+
+def save_images():
+    _, _, X = load_data()
     prediction = np.load('prediction.npy')
     for i in range(len(prediction)):
         plt.clf()
@@ -54,44 +63,45 @@ def model():
     return cnn
 
 
-with np.load('prediction-challenge-02-data.npz') as fh:
-    x_train = fh['x_train']
-    y_train = fh['y_train']
-    x_test = fh['x_test']
+if __name__ == '__main__':
+    with np.load('prediction-challenge-02-data.npz') as fh:
+        x_train = fh['x_train']
+        y_train = fh['y_train']
+        x_test = fh['x_test']
 
-# TRAINING DATA: INPUT (x) AND OUTPUT (y)
-# 1. INDEX: IMAGE SERIAL NUMBER (6000)
-# 2/3. INDEX: PIXEL VALUE (32 x 32)
-# 4. INDEX: COLOR CHANNELS (3)
-print(x_train.shape, x_train.dtype)
-print(y_train.shape, y_train.dtype)
-# TEST DATA: INPUT (x) ONLY
-print(x_test.shape, x_test.dtype)
-# split dataset
-X_train, X_val, Y_train, Y_val = train_test_split(x_train,
-                                                  y_train,
-                                                  test_size=.2)
-# TRAIN MODEL ON x_train, y_train
-debug = False
-if debug:
-    ep = 5
-else:
-    ep = 20
-cnn = model()
-cnn.fit(x_train, y_train, epochs=ep)
+    # TRAINING DATA: INPUT (x) AND OUTPUT (y)
+    # 1. INDEX: IMAGE SERIAL NUMBER (6000)
+    # 2/3. INDEX: PIXEL VALUE (32 x 32)
+    # 4. INDEX: COLOR CHANNELS (3)
+    print(x_train.shape, x_train.dtype)
+    print(y_train.shape, y_train.dtype)
+    # TEST DATA: INPUT (x) ONLY
+    print(x_test.shape, x_test.dtype)
+    # split dataset
+    X_train, X_val, Y_train, Y_val = train_test_split(x_train,
+                                                      y_train,
+                                                      test_size=.2)
+    # TRAIN MODEL ON x_train, y_train
+    debug = False
+    if debug:
+        ep = 5
+    else:
+        ep = 50
+    cnn = model()
+    cnn.fit(x_train, y_train, epochs=ep)
 
-cnn.evaluate(X_val, Y_val)
-y_predict = cnn.predict(x_test)
-y_classes = [np.argmax(element) for element in y_predict]
-y_cla = np.array(y_classes)
-y_cla.astype(np.int32)
-y_cla = y_cla.tolist()
-np.save('prediction.npy', y_cla)
-validate_prediction_format()
-# plot_sample(x_test, y_classes, index)
-# PREDICT prediction FROM x_test
+    cnn.evaluate(X_val, Y_val)
+    y_predict = cnn.predict(x_test)
+    y_classes = [np.argmax(element) for element in y_predict]
+    y_cla = np.array(y_classes)
+    y_cla.astype(np.int32)
+    y_cla = y_cla.tolist()
+    np.save('prediction.npy', y_cla)
+    validate_prediction_format()
+    # plot_sample(x_test, y_classes, index)
+    # PREDICT prediction FROM x_test
 
-# AND SAVE EXACTLY AS SHOWN BELOW
-# np.save('prediction.npy', prediction.astype(int))
+    # AND SAVE EXACTLY AS SHOWN BELOW
+    # np.save('prediction.npy', prediction.astype(int))
 
-# validate_prediction_format()
+    # validate_prediction_format()
